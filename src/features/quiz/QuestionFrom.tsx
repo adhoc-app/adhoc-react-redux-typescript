2e972f212a86f1f02e221724d5873522503ddd14
Huyen Truong <huyen@free.fi>
2021-05-25 23:53:49 +0300

    question_card Add question form component

---
+++ src/features/quiz/QuestionFrom.tsx
@@ -0,0 +1,39 @@
+import {
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+} from "@material-ui/core";
+import React, { useState } from "react";
+import { Option } from "../../app/types";
+
+type QuestionFormProps = {
+  options: Option[];
+  prompt: string;
+};
+
+function QuestionForm({ options, prompt }: QuestionFormProps) {
+  const [selected, setSelected] = useState("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelected((event.target as HTMLInputElement).value);
+  };
+  return (
+    <FormControl component="fieldset">
+      <FormLabel component="legend">{prompt}</FormLabel>
+      <RadioGroup
+        aria-label="gender"
+        name="gender1"
+        value={selected}
+        onChange={handleChange}
+      >
+        {options.map(({ value, label }) => (
+          <FormControlLabel value={value} control={<Radio />} label={label} />
+        ))}
+      </RadioGroup>
+    </FormControl>
+  );
+}
+
+export default QuestionForm;
