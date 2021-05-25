79aeebb07774375009606ad57937429fc787d5c8
Huyen Truong <huyen@free.fi>
2021-05-26 00:20:34 +0300

    question_card Add button and its onClick handler

--- src/features/quiz/QuestionFrom.tsx
+++ src/features/quiz/QuestionFrom.tsx
@@ -1,6 +1,8 @@
 import {
+  Button,
   FormControl,
   FormControlLabel,
+  FormHelperText,
   FormLabel,
   Radio,
   RadioGroup,
@@ -16,24 +18,41 @@ type QuestionFormProps = {
 
 function QuestionForm({ index, options, prompt }: QuestionFormProps) {
   const [selected, setSelected] = useState("");
+  const [helperText, setHelperText] = useState("");
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSelected((event.target as HTMLInputElement).value);
   };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setHelperText("Clicked");
+  };
+
   return (
-    <FormControl component="fieldset">
-      <FormLabel component="legend">{prompt}</FormLabel>
-      <RadioGroup
-        aria-label="gender"
-        name="gender1"
-        value={selected}
-        onChange={handleChange}
-      >
-        {options.map(({ value, label }) => (
-          <FormControlLabel value={value} control={<Radio />} label={label} />
-        ))}
-      </RadioGroup>
-    </FormControl>
+    <form onSubmit={handleSubmit}>
+      <FormControl component="fieldset">
+        <FormLabel component="legend">{prompt}</FormLabel>
+        <RadioGroup
+          aria-label={`question${index}`}
+          name={`question${index}`}
+          value={selected}
+          onChange={handleChange}
+        >
+          {options.map(({ value, label }) => (
+            <FormControlLabel
+              value={value.toString()}
+              control={<Radio />}
+              label={label}
+            />
+          ))}
+        </RadioGroup>
+        <FormHelperText>{helperText}</FormHelperText>
+        <Button type="submit" variant="outlined" color="primary">
+          Check Answer
+        </Button>
+      </FormControl>
+    </form>
   );
 }
 
