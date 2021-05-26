f178050d66066a1f73bd9a4741be5f8f70f6dc0e
Huyen Truong <huyen@free.fi>
2021-05-26 23:52:55 +0300

    question_card Remove one mock question

--- src/features/quiz/Quiz.tsx
+++ src/features/quiz/Quiz.tsx
@@ -3,15 +3,6 @@ import Card from "./Card";
 
 const mockQuestions = [
   {
-    prompt: "ăn cơm",
-    options: [
-      { value: 1, label: "sleep" },
-      { value: 2, label: "write" },
-      { value: 3, label: "eat" },
-      { value: 4, label: "work" },
-    ],
-  },
-  {
     prompt: "Con mèo trèo cây cau.",
     options: [
       { value: 1, label: "The cat climbs on a tall tree." },
