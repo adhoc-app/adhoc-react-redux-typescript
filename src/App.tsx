f329d08c6b1359a3ad7eaf2ddd181c19cfebbd8f
Toni Nyberg <toninyberg92@gmail.com>
2021-05-28 21:15:40 +0300

    text-input Add header

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Quiz from "./features/quiz/Quiz";
 function App() {
   return (
     <div className="App">
+      {/* <Header /> */}
       <Quiz />
     </div>
   );
---
+++ src/features/quiz/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-green-400 to-cyan-500">
+            <h1 className="font-bold text-3xl text-gray-500">
+                Ad Hoc App
+            </h1>
+        </div>
+    )
+}
+
+export default Header
