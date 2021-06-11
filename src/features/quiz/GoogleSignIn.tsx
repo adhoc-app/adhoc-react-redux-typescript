132bf87e304adf85947d5d849c71103564ddda14
Toni Nyberg <toninyberg92@gmail.com>
2021-06-11 19:41:48 +0300

    navbar removed useless text from the google sign in button

--- src/features/quiz/GoogleSignIn.tsx
+++ src/features/quiz/GoogleSignIn.tsx
@@ -12,57 +12,55 @@ interface GoogleSignInProps {
   //   ) => void;
 }
 
-export const GoogleSignIn: FunctionComponent<GoogleSignInProps> = (
+export const GoogleSignIn: FunctionComponent<GoogleSignInProps> = () =>
+  // {
+  //   //   loginSuccess,
+  // }
   {
-    //   loginSuccess,
-  }
-) => {
-  const [loginFailed, setLoginFailed] = useState<boolean>();
-  const [showloginButton, setShowloginButton] = useState(true);
-  const [showlogoutButton, setShowlogoutButton] = useState(false);
-  const clientId =
-    "843337520358-orksjjfgg1v13982b6n0pv3mis8htmoa.apps.googleusercontent.com";
-  const onSignoutSuccess = () => {
-    alert("You have been logged out successfully");
-    console.clear();
-    setShowloginButton(true);
-    setShowlogoutButton(false);
-  };
-  const onLoginSuccess = (
-    res: GoogleLoginResponse | GoogleLoginResponseOffline
-  ) => {
-    console.log("Login Success:", res);
-    setShowloginButton(false);
-    setShowlogoutButton(true);
-  };
+    const [loginFailed, setLoginFailed] = useState<boolean>();
+    const [showloginButton, setShowloginButton] = useState(true);
+    const [showlogoutButton, setShowlogoutButton] = useState(false);
+    const clientId =
+      "843337520358-orksjjfgg1v13982b6n0pv3mis8htmoa.apps.googleusercontent.com";
+    const onSignoutSuccess = () => {
+      alert("You have been logged out successfully");
+      console.clear();
+      setShowloginButton(true);
+      setShowlogoutButton(false);
+    };
+    const onLoginSuccess = (
+      res: GoogleLoginResponse | GoogleLoginResponseOffline
+    ) => {
+      console.log("Login Success:", res);
+      setShowloginButton(false);
+      setShowlogoutButton(true);
+    };
 
-  return (
-    <div className="text-center mb-4">
-      <h1 className="h3 mb-3 font-weight-normal">Welcome to Library Portal</h1>
-      {loginFailed && <p>"Could not sign you in! Try again."</p>}
-      <p>Sign In</p>
-      {showloginButton ? (
-        <GoogleLogin
-          clientId={clientId}
-          buttonText="Google"
-          onSuccess={onLoginSuccess}
-          onFailure={(response: any) => {
-            setLoginFailed(true);
-          }}
-          cookiePolicy={"single_host_origin"}
-          responseType="code,token"
-        />
-      ) : null}
-      {showlogoutButton ? (
-        <GoogleLogout
-          clientId={clientId}
-          buttonText="Sign Out"
-          onLogoutSuccess={onSignoutSuccess}
-        ></GoogleLogout>
-      ) : null}
-    </div>
-  );
-};
+    return (
+      <div className="text-center mb-4">
+        {loginFailed && <p>"Could not sign you in! Try again."</p>}
+        {showloginButton ? (
+          <GoogleLogin
+            clientId={clientId}
+            buttonText="Google"
+            onSuccess={onLoginSuccess}
+            onFailure={(response: any) => {
+              setLoginFailed(true);
+            }}
+            cookiePolicy={"single_host_origin"}
+            responseType="code,token"
+          />
+        ) : null}
+        {showlogoutButton ? (
+          <GoogleLogout
+            clientId={clientId}
+            buttonText="Sign Out"
+            onLogoutSuccess={onSignoutSuccess}
+          ></GoogleLogout>
+        ) : null}
+      </div>
+    );
+  };
 //
 
 // function Login() {
